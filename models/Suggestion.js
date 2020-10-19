3d53c05342bcfa894479efc3e6b5b09c097c03da
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-19 09:43:50 -0400

    schema added

--- models/Suggestion.js
+++ models/Suggestion.js
@@ -1,6 +1,13 @@
 const mongoose = require ('mongoose');
 
 const suggestionSchema = new mongoose.Schema({
-    title:{type:String,lowercase:true, unique:true},
-    
-})
+    title:{type:String,lowercase:true, unique:true, required:true},
+    name:{type:String, lowercase:true, required:true},
+    suggestion:{type:String, lowercase:true, required:true},
+    likes:{type:Number, default:0},
+    anonymous:{type: Boolean},
+    timeCreated:{type:Date}
+});
+
+
+module.exports= mongoose.model('suggestions', suggestionSchema)()
