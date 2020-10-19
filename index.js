242878434abe1065eb95fa69ce575040e17b88a2
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-19 08:52:35 -0400

    updates

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express =require('express');
 const app = express();
+const mongoose =require('mongoose');
+const morgan = require('morgan');
 
 require('dotenv').config();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+.then(()=> console.log('MongoDB Connected')).catch((err)=> console.log(`MongoDB Error ${err}`))
+
+app.use(morgan('dev'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/', (req, res)=>{
     res.send('Server is working')
