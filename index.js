91ec639c1069dd95bd38535f60dfdc55a128ff8b
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-10-19 09:51:05 -0400

    updated parent route

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-
-app.use('/suggestion',router)
+//parent route
+app.use('api/v1/suggestions',router)
 
 // app.get('/', (req, res)=>{
 //     res.send('Server is working')
